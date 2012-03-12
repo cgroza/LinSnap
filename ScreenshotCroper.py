e6cace794fed035781c738d02ab1690a812965f7
Groza Cristian <kristi9524@gmail.com>
2012-03-12 19:08:25 -0400

    Added a menubar to the ScreenshotCroper. The screenshots are no longer scaled.

--- ScreenshotCroper.py
+++ ScreenshotCroper.py
@@ -6,30 +6,32 @@ from ViewScreenshot import *
 class ScreenshotCroper(ViewScreenshot):
     def __init__(self, parent, id = -1):
         ViewScreenshot.__init__(self, parent, id)
+        self.menubar = wx.MenuBar( ) 
+        self.scrnshot = wx.Menu()
+        self.scrnshot.Append(1000, "Crop")
+        self.scrnshot.Append(1010, "Save")
+        self.scrnshot.Append(1020, "Reset")
+        self.scrnshot.Append(1030, "Close")
+        self.menubar.Append(self.scrnshot, "Screenshot")
+        self.SetMenuBar(self.menubar)
+
+        self.Bind(wx.EVT_MENU, self.OnCrop, id = 1000)
+        self.Bind(wx.EVT_MENU, self.OnSave , id = 1010)
+        self.Bind(wx.EVT_MENU, self.OnReset , id = 1020)
+        self.Bind(wx.EVT_MENU, self.OnClose , id = 1030)
 
         # Create the rubberband
         self.rubber_band = rubberband.RubberBand(drawingSurface = self.image_ctrl)
         self.rubber_band.reset(aspectRatio=0.5)
-        self.crop_btn = wx.Button(self.panel, -1, "Crop")
-        self.save_btn = wx.Button(self.panel, -1, "Save")
-
-        self.crop_btn.Bind(wx.EVT_BUTTON, self.OnCrop)
-        self.save_btn.Bind(wx.EVT_BUTTON, self.OnSave)
-
-        button_sizer = wx.BoxSizer(wx.VERTICAL)
-        button_sizer.Add(self.crop_btn, 0, wx.ALL|wx.EXPAND)
-        button_sizer.Add(self.save_btn, 0, wx.ALL|wx.EXPAND)
-        self.main_sizer.Add(button_sizer, 1, wx.EXPAND|wx.CENTER)
-
-        self.Fit()
 
     def OnSave(self, event):
-        w = self.image.GetWidth()
-        h = self.image.GetHeight()
-        save_img = self.image.Scale(w / (float(2) / 3), h / (float (2) /3))
-        self.image.SaveFile(self.current_file, wx.BITMAP_TYPE_PNG)
+        self.image.SaveFile(self.current_element.get("path"), wx.BITMAP_TYPE_PNG)
         event.Skip()
-
+        
+    def OnReset(self, event):
+        self.ShowScrnshot(self.current_element)
+        event.Skip()
+        
     def CropImage(self):
         coords = self.rubber_band.getCurrentExtent()
         if coords is None:
@@ -40,7 +42,6 @@ class ScreenshotCroper(ViewScreenshot):
         height = y1 - y0
 
         self.image = self.image.GetSubImage(wx.Rect(x0, y0, width, height))
-
         self.image_ctrl.SetBitmap(wx.BitmapFromImage(self.image))
         self.rubber_band.reset()
 
@@ -51,5 +52,4 @@ class ScreenshotCroper(ViewScreenshot):
     def ShowScrnshot(self, element):
         ViewScreenshot.ShowScrnshot(self, element)
         sz = self.GetSize()
-        self.SetSize((sz.GetWidth() + 200, sz.GetHeight()))
         self.rubber_band.reset()
--- ViewScreenshot.py
+++ ViewScreenshot.py
@@ -5,8 +5,7 @@ class ViewScreenshot(wx.Frame):
     def __init__(self, parent, id = -1, title = 'Screenshot Viewer' ):
         wx.Frame.__init__(self, parent, id, title)
         self.panel = wx.Panel(self)
-        self.photo_max_size = 240
-        self.current_file = ""
+        self.current_element = None
         self.__create_gui()
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         
@@ -14,7 +13,6 @@ class ViewScreenshot(wx.Frame):
         self.image = wx.EmptyImage(240,240)
         self.image_ctrl = wx.StaticBitmap(self.panel, wx.ID_ANY,
                                        wx.BitmapFromImage(self.image))
-
 
         self.main_sizer = wx.BoxSizer(wx.HORIZONTAL)
         self.main_sizer.Add(self.image_ctrl, 0, wx.ALL|wx.EXPAND)
@@ -26,18 +24,11 @@ class ViewScreenshot(wx.Frame):
         
 
     def ShowScrnshot(self, element):
-        self.current_file = element.get("path")
-        self.image = wx.Image(self.current_file, wx.BITMAP_TYPE_ANY)
+        self.current_element = element
+        self.image = wx.Image(self.current_element.get("path"), wx.BITMAP_TYPE_ANY)
         # scale the image, preserving the aspect ratio
         w = self.image.GetWidth()
         h = self.image.GetHeight()
-        dsize = wx.GetDisplaySize()
-        if w >= dsize.GetWidth() or h >= dsize.GetHeight():
-            h = dsize.GetHeight() * (float(2) / 3)
-            w = dsize.GetWidth() * (float(2) / 3)
-            self.image = self.image.Scale(w,h)        
-
-
         self.image_ctrl.SetBitmap(wx.BitmapFromImage(self.image))
         self.panel.Refresh()
         self.SetSize((w,h))
